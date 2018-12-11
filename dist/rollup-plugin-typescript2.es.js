c2916342e8812f81cec749c3b5c12bc4503521f0
Eugene Zolenko <zolenkoe@gmail.com>
2018-12-10 17:18:05 -0700

    - package version

--- dist/rollup-plugin-typescript2.es.js
+++ dist/rollup-plugin-typescript2.es.js
@@ -21474,7 +21474,7 @@ function typescript(options) {
             context.info(`tslib version: ${tslibVersion}`);
             if (!semver_30(tsModule.version, ">=2.4.0", { includePrerelease: true }))
                 throw new Error(`Installed typescript version '${tsModule.version}' is outside of supported range '>=2.4.0'`);
-            context.info(`rollup-plugin-typescript2 version: 0.18.1`);
+            context.info(`rollup-plugin-typescript2 version: 0.18.2`);
             context.debug(() => `plugin options:\n${JSON.stringify(pluginOptions, (key, value) => key === "typescript" ? `version ${value.version}` : value, 4)}`);
             context.debug(() => `rollup config:\n${JSON.stringify(rollupOptions, undefined, 4)}`);
             watchMode = process.env.ROLLUP_WATCH === "true";
