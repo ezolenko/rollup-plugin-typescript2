0049f1634e31507138b10e41dfa62a5c8951022e
Anton Gilgur <agilgur5@gmail.com>
2022-05-03 20:34:33 -0400

    fix(test): update host spec to match latest source
    - LanguageServiceHost now requires 3rd argument, cwd, so add that to
      all usage
    - add afterDecalarations to transformers
    
    - Jest's test.skip can now use async/await, so do that
      - it was also giving a type error, and this is simpler anyway
    
    - change expected to use `__tests__` directory now
    
    - fix: use `expect.arrayContaining` for `host.getDirectories` test as
      there are more temp directories that get added here, such as
      `build-self` and `coverage`
      - in general, might be less fragile if this used a generated directory
        instead of an actual one
        - but save bigger refactors for later

--- __tests__/host.spec.ts
+++ __tests__/host.spec.ts
@@ -5,6 +5,7 @@ import { readFile, remove, ensureDir, writeFile } from "fs-extra";
 import { LanguageServiceHost } from "../src/host";
 
 const local = (x: string) => path.resolve(__dirname, x);
+const cwd = local("");
 const unaryFunctionExample = "const unary = (x: string): string => x.reverse()";
 
 afterAll(() =>
@@ -32,16 +33,16 @@ test("LanguageServiceHost", done => {
 		options: { test: "this is a test" }
 	};
 	const transformers = [() => ({})];
-	const host = new LanguageServiceHost(config, transformers);
+	const host = new LanguageServiceHost(config, transformers, cwd);
 
 	expect(host).toBeTruthy();
 	expect(Object.keys(host)).toEqual([
 		"parsedConfig",
 		"transformers",
-		"cwd",
 		"snapshots",
 		"versions",
-		"fileNames"
+		"fileNames",
+		"cwd",
 	]);
 
 	host.reset();
@@ -66,17 +67,17 @@ test("LanguageServiceHost", done => {
 			.join(path.sep);
 	const truncate3 = truncateName(-3);
 	expect([...(host as any).fileNames].map(truncate3)).toEqual([
-		"rollup-plugin-typescript2/src/test.ts"
+		"rollup-plugin-typescript2/__tests__/test.ts"
 	]);
 	expect(host.getScriptSnapshot(testFile)).toEqual({
 		text: unaryFunctionExample
 	});
 	expect(host.getScriptSnapshot(local("does-not-exist.ts"))).toBeFalsy();
-	expect(host.getCurrentDirectory()).toEqual(path.resolve(__dirname, ".."));
+	expect(host.getCurrentDirectory()).toEqual(cwd);
 	expect(host.getScriptVersion(testFile)).toEqual("1");
 	expect(host.getScriptVersion("nothing")).toEqual("0");
 	expect(host.getScriptFileNames().map(truncate3)).toEqual([
-		"rollup-plugin-typescript2/src/test.ts"
+		"rollup-plugin-typescript2/__tests__/test.ts"
 	]);
 	expect(host.getCompilationSettings()).toEqual({ test: "this is a test" });
 	expect(truncateName(-5)(host.getDefaultLibFileName({}))).toEqual(
@@ -89,19 +90,20 @@ test("LanguageServiceHost", done => {
 	}
 	expect(host.getTypeRootsVersion()).toEqual(0);
 	expect(host.directoryExists("no-it-does-not")).toBeFalsy();
-	expect(host.getDirectories(".")).toEqual([
+	expect(host.getDirectories(".")).toEqual(expect.arrayContaining([
 		".git",
 		".github",
+		"__tests__",
 		"dist",
 		"node_modules",
 		"src"
-	]);
-	expect(host.getCustomTransformers()).toEqual({ after: [], before: [] });
+	]));
+	expect(host.getCustomTransformers()).toEqual({ after: [], afterDeclarations: [], before: [] });
 	expect(host.fileExists("no-it-does.not")).toBeFalsy();
 	writeFile(local("does-exist.ts"), unaryFunctionExample, "utf8").then(() => {
 		expect(
 			host.readDirectory(local("host-test-dir")).map(truncate3)
-		).toEqual(["src/host-test-dir/host-test-file.ts"]);
+		).toEqual(["__tests__/host-test-dir/host-test-file.ts"]);
 		expect(host.fileExists(local("does-exist.ts"))).toBeTruthy();
 		expect(host.getScriptSnapshot(local("does-exist.ts"))).toEqual({
 			text: unaryFunctionExample
@@ -117,7 +119,7 @@ test("LanguageServiceHost.readFile", () => {
 		options: { test: "this is a test" }
 	};
 	const transformers = [() => ({})];
-	const host = new LanguageServiceHost(config, transformers);
+	const host = new LanguageServiceHost(config, transformers, cwd);
 
 	expect(
 		host.readFile(local("src/host-test-dir/host-test-file.js"))
@@ -125,8 +127,8 @@ test("LanguageServiceHost.readFile", () => {
 });
 
 // ts.sys.readFile() doesn't ever appear to return anything, so skipping this test for now
-test.skip("LanguageServiceHost.readFile", done => {
-	return readFile(local("host-test-dir/host-test-file.ts"), "utf8").then(
+test.skip("LanguageServiceHost.readFile", async () => {
+	await readFile(local("host-test-dir/host-test-file.ts"), "utf8").then(
 		data => {
 			expect(data).toEqual(unaryFunctionExample);
 
@@ -136,7 +138,7 @@ test.skip("LanguageServiceHost.readFile", done => {
 				options: { test: "this is a test" }
 			};
 			const transformers = [() => ({})];
-			const host = new LanguageServiceHost(config, transformers);
+			const host = new LanguageServiceHost(config, transformers, cwd);
 
 			// const file = host.readFile(local("host-test-dir/host-test-file.js"));
 			// expect(file).toEqual(unaryFunctionExample);
@@ -144,8 +146,6 @@ test.skip("LanguageServiceHost.readFile", done => {
 			// expect(file2).toEqual(unaryFunctionExample);
 			const file3 = host.readFile("src/host-test-dir/host-test-file.js");
 			expect(file3).toEqual(unaryFunctionExample);
-
-			done();
 		}
 	);
 });
@@ -157,7 +157,7 @@ test("LanguageServiceHost - getCustomTransformers", () => {
 		options: {}
 	};
 	const transformers = [() => ({ before: () => "test" })];
-	const host = new LanguageServiceHost(config, transformers as any);
+	const host = new LanguageServiceHost(config, transformers as any, cwd);
 	const customTransformers = host.getCustomTransformers();
 
 	expect(customTransformers).toBeFalsy();
@@ -170,7 +170,7 @@ test.skip("LanguageServiceHost - getCustomTransformers with no service", () => {
 		options: {}
 	};
 	const transformers = [() => ({ before: () => "test" })];
-	const host = new LanguageServiceHost(config, transformers as any);
+	const host = new LanguageServiceHost(config, transformers as any, cwd);
 	const I = (x: any) => x;
 	host.setLanguageService(I as any);
 	const customTransformers = host.getCustomTransformers();
