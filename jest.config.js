20f66e9f17ff4623c90dead9c43618435886f720
Anton Gilgur <agilgur5@gmail.com>
2022-05-07 16:38:00 -0400

    lint: add __tests__ to lint dirs, fix lint errors
    - surprisingly only in jest.config.js?
    
    - really need to update to @typescript-eslint ...

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,10 @@
 /** @type {import('@jest/types').Config.InitialOptions} */
 const config = {
-  preset: 'ts-jest',
-  injectGlobals: false, // use @jest/globals instead
-  restoreMocks: true,
-  // only use *.spec.ts files in __tests__, no auto-generated files
-  testMatch: ["**/__tests__/**/*.spec.ts?(x)"]
+	preset: 'ts-jest',
+	injectGlobals: false, // use @jest/globals instead
+	restoreMocks: true,
+	// only use *.spec.ts files in __tests__, no auto-generated files
+	testMatch: ["**/__tests__/**/*.spec.ts?(x)"]
 };
 
 module.exports = config;
