a118655641330e659071c80d46295ff073eaefd8
Anton Gilgur <agilgur5@gmail.com>
2022-05-09 23:41:50 -0400

    test: 100% coverage for host.ts, refactor host.spec.ts
    - test: get the skipped `readFile` test to work
      - `ts.sys.readFile` definitely returns stuff, it's used for snapshots
      - it wasn't working because it was testing the wrong extension,
        `.js` instead of `.ts`
        - I thought this was intentional when I first added consts here, but
          turns out that was actually a mistake by the original author
    - refactor: merge the `readFile` tests with the main test suite
      - first I merged them together, but as they were just one-liners, I
        merged them with the first set, since it wasn't particularly
        different from those
    
    - refactor: split up the main test suite into a few commented sections
      - core snapshot functionality, fs functionality, misc
      - a lot less random now
    - refactor: use `getDirectories` on the testDir instead of project root
      - much less fragile to use the dir generated here, and no hack-ish
        `arrayContaining()` either
    - refactor: one-line the case-insensitive test
      - that simplifies it a lot!
    - refactor: use fixed consts for the repetitive default config
      - and use a `testOpts` variable for less fragile testing
    
    - test: add tests for version 2 branches of snapshot funcs
      - these weren't tested before so it had only tested the `|| 0` branch
        of the conditional
      - also actually test `reset` instead of calling it at the beginning
    - refactor: no more "dirty checking" of instances' private interfaces
      - we're testing it all through the public interfaces instead!
    
    - test: add a simple test for the new `trace` method
    
    - test: coverage for `after` and `afterDeclarations` transformers
    - test: coverage for different `undefined` states of transformers
      - refactor the two test suites as one for `undefined` and one for
        all 3 transformers
      - and put the working one first for naming purposes
      - refactor the `setLanguageService` call as a simpler type coercion
      - remove the transformer interactions in the main test suite
    
    - yea, this is a big commit, but I was refactoring everything anyway and
      this is probably gonna be squashed anyway

--- __tests__/host.spec.ts
+++ __tests__/host.spec.ts
@@ -1,23 +1,21 @@
-import { afterAll, beforeAll, test, expect } from "@jest/globals";
+import { afterAll, beforeAll, test, expect, jest } from "@jest/globals";
 import * as ts from "typescript";
 import * as path from "path";
-import { readFile, remove, ensureDir, writeFile } from "fs-extra";
+import { remove, ensureDir, writeFile } from "fs-extra";
 
 import { setTypescriptModule } from "../src/tsproxy";
 import { LanguageServiceHost } from "../src/host";
 
 setTypescriptModule(ts);
 
-const local = (x: string) => path.resolve(__dirname, x);
+const defaultConfig = { fileNames: [], errors: [], options: {} };
 
 const unaryFunc = "const unary = (x: string): string => x.reverse()";
 const unaryFuncSnap = { text: unaryFunc };
 
+const local = (x: string) => path.resolve(__dirname, x);
 const testDir = local("__temp/host");
-const testFileNoExt = `${testDir}/file`;
-const testFile = `${testFileNoExt}.ts`;
-const testFileJs = `${testFileNoExt}.js`;
-
+const testFile = `${testDir}/file.ts`;
 const nonExistent = `${testDir}/this-does-not-exist.ts`;
 
 afterAll(() => remove(testDir));
@@ -27,123 +25,85 @@ beforeAll(async () => {
 });
 
 test("LanguageServiceHost", async () => {
-	const config = {
-		fileNames: [],
-		errors: [],
-		options: { test: "this is a test" }
-	};
+	const testOpts = { test: "this is a test" };
+	const config = { ...defaultConfig, options: testOpts };
 	const transformers = [() => ({})];
 	const host = new LanguageServiceHost(config, transformers, testDir);
 
-	expect(host).toBeTruthy();
-	expect(Object.keys(host)).toEqual([
-		"parsedConfig",
-		"transformers",
-		"snapshots",
-		"versions",
-		"fileNames",
-		"cwd",
-	]);
-
-	host.reset();
-	expect((host as any).snapshots).toEqual({});
-	expect((host as any).versions).toEqual({});
-
-	host.setLanguageService({ pretend: "language-service" } as any);
-	expect((host as any).service).toEqual({ pretend: "language-service" });
-
-	const snap = host.setSnapshot(testFile, unaryFunc);
-	expect(snap).toEqual(unaryFuncSnap);
-	expect((host as any).snapshots[testFile]).toEqual(unaryFuncSnap);
-	expect((host as any).versions[testFile]).toEqual(1);
-
-	expect([...(host as any).fileNames]).toEqual([testFile]);
+	// test core snapshot functionality
 	expect(host.getScriptSnapshot(testFile)).toEqual(unaryFuncSnap);
-	expect(host.getScriptSnapshot(nonExistent)).toBeFalsy();
-	expect(host.getCurrentDirectory()).toEqual(testDir);
 	expect(host.getScriptVersion(testFile)).toEqual("1");
+
+	expect(host.setSnapshot(testFile, unaryFunc)).toEqual(unaryFuncSnap); // version 2
+	expect(host.getScriptSnapshot(testFile)).toEqual(unaryFuncSnap); // get from dict
+	expect(host.getScriptVersion(testFile)).toEqual("2");
+
+	expect(host.getScriptSnapshot(nonExistent)).toBeFalsy();
 	expect(host.getScriptVersion(nonExistent)).toEqual("0");
+
 	expect(host.getScriptFileNames()).toEqual([testFile]);
-	expect(host.getCompilationSettings()).toEqual({ test: "this is a test" });
+
+	host.reset(); // back to version 1
+	expect(host.setSnapshot(testFile, unaryFunc)).toEqual(unaryFuncSnap);
+	expect(host.getScriptVersion(testFile)).toEqual("1");
+
+	// test fs functionality, which just uses tsModule.sys equivalents
+	expect(host.getCurrentDirectory()).toEqual(testDir);
+	expect(host.getDirectories(testDir)).toEqual([]);
+	expect(host.directoryExists(nonExistent)).toBeFalsy();
+	expect(host.fileExists(nonExistent)).toBeFalsy();
+	expect(host.fileExists(testFile)).toBeTruthy();
+	expect(host.readDirectory(testDir)).toEqual([testFile]);
+	expect(host.readFile(nonExistent)).toBeFalsy();
+	expect(host.readFile(testFile)).toEqual(unaryFunc);
+	expect(host.useCaseSensitiveFileNames()).toBe(process.platform === "win32");
+
+	// test misc functionality
+	expect(host.getCompilationSettings()).toEqual(testOpts);
 	expect(host.getDefaultLibFileName({})).toEqual(
 		local("../node_modules/typescript/lib/lib.d.ts")
 	);
-	if (process.platform === "win32") {
-		expect(host.useCaseSensitiveFileNames()).toBeTruthy();
-	} else {
-		expect(host.useCaseSensitiveFileNames()).toBeFalsy();
-	}
 	expect(host.getTypeRootsVersion()).toEqual(0);
-	expect(host.directoryExists(nonExistent)).toBeFalsy();
-	expect(host.getDirectories(".")).toEqual(expect.arrayContaining([
-		".git",
-		".github",
-		"__tests__",
-		"dist",
-		"node_modules",
-		"src"
-	]));
-	expect(host.getCustomTransformers()).toEqual({ after: [], afterDeclarations: [], before: [] });
-	expect(host.fileExists(nonExistent)).toBeFalsy();
-	expect(host.readDirectory(testDir)).toEqual([testFile]);
-	expect(host.fileExists(testFile)).toBeTruthy();
+
+	// mock out trace
+	console.log = jest.fn();
+	host.trace('test log');
+	expect(console.log).toHaveBeenCalledWith('test log');
 });
 
-test("LanguageServiceHost.readFile", () => {
-	const config = {
-		fileNames: [],
-		errors: [],
-		options: { test: "this is a test" }
-	};
-	const transformers = [() => ({})];
-	const host = new LanguageServiceHost(config, transformers, testDir);
-
-	expect(host.readFile(testFileJs)).toBeFalsy();
-});
-
-// ts.sys.readFile() doesn't ever appear to return anything, so skipping this test for now
-test.skip("LanguageServiceHost.readFile", async () => {
-	const data = await readFile(testFile, "utf8");
-	expect(data).toEqual(unaryFunc);
-
-	const config = {
-		fileNames: [],
-		errors: [],
-		options: { test: "this is a test" }
-	};
-	const transformers = [() => ({})];
-	const host = new LanguageServiceHost(config, transformers, testDir);
-
-	const file = host.readFile(testFileJs);
-	expect(file).toEqual(unaryFunc);
-});
 
 test("LanguageServiceHost - getCustomTransformers", () => {
-	const config = {
-		fileNames: [],
-		errors: [],
-		options: {}
-	};
-	const transformers = [() => ({ before: () => "test" })];
+	const config = { ...defaultConfig };
+	const transformers = [() => ({
+		before: () => "testBefore",
+		after: () => "testAfter",
+		afterDeclarations: () => "testAfterDeclarations",
+	})];
 	const host = new LanguageServiceHost(config, transformers as any, testDir);
-	const customTransformers = host.getCustomTransformers();
 
-	expect(customTransformers).toBeFalsy();
+	host.setLanguageService(true as any);
+	const customTransformers = host.getCustomTransformers();
+	// tiny helper for all the type coercion etc
+	const callTransform = (type: 'before' | 'after' | 'afterDeclarations') => {
+		return customTransformers?.[type]?.[0](true as any);
+	}
+
+	expect(callTransform('before')).toEqual(transformers[0]().before());
+	expect(callTransform('after')).toEqual(transformers[0]().after());
+	expect(callTransform('afterDeclarations')).toEqual(transformers[0]().afterDeclarations());
 });
 
-test("LanguageServiceHost - getCustomTransformers with no service", () => {
-	const config = {
-		fileNames: [],
-		errors: [],
-		options: {}
-	};
-	const transformers = [() => ({ before: () => "test" })];
-	const host = new LanguageServiceHost(config, transformers as any, testDir);
-	const I = (x: any) => x;
-	host.setLanguageService(I as any);
-	const customTransformers = host.getCustomTransformers();
+test("LanguageServiceHost - getCustomTransformers -- undefined cases", () => {
+	const config = { ...defaultConfig };
 
-	expect(customTransformers?.before?.[0].toString()).toEqual(
-		transformers[0]().before.toString()
-	);
+	// no LS and no transformers cases
+	let host = new LanguageServiceHost(config, undefined as any, testDir);
+	expect(host.getCustomTransformers()).toBeFalsy(); // no LS
+	host.setLanguageService(true as any);
+	expect(host.getCustomTransformers()).toBeFalsy(); // no transformers
+
+	// empty transformers case
+	host = new LanguageServiceHost(config, [], testDir);
+	host.setLanguageService(true as any);
+	expect(host.getCustomTransformers()).toBeFalsy(); // empty transformers
 });
