529c8530f5e050a91da18ecb009cbde12ad1ef72
ezolenko <zolenkoe@gmail.com>
2022-05-16 15:13:44 -0600

    - fix for linter

--- __tests__/tslib.spec.ts
+++ __tests__/tslib.spec.ts
@@ -4,8 +4,8 @@ import * as fs from "fs-extra";
 import { tslibVersion, tslibSource } from "../src/tslib";
 
 test("tslib", async () => {
-  expect(tslibVersion).toEqual(require("tslib/package.json").version);
+	expect(tslibVersion).toEqual(require("tslib/package.json").version);
 
-  const tslibES6 = await fs.readFile(require.resolve("tslib/tslib.es6.js"), "utf8");
-  expect(tslibSource).toEqual(tslibES6);
+	const tslibES6 = await fs.readFile(require.resolve("tslib/tslib.es6.js"), "utf8");
+	expect(tslibSource).toEqual(tslibES6);
 });
