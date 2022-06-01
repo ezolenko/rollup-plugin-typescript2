43108732dfaace0a389550edef75b09996d313b9
ezolenko <zolenkoe@gmail.com>
2022-05-31 20:54:36 -0600

    - fix for wrong this #333

--- src/tscache.ts
+++ src/tscache.ts
@@ -264,7 +264,7 @@ export class TsCache
 		if (this.ambientTypesDirty)
 			this.context.info(yellow("ambient types changed, redoing all semantic diagnostics"));
 
-		typeNames.forEach(this.typesCache.touch, this);
+		typeNames.forEach(this.typesCache.touch, this.typesCache);
 	}
 
 	private getDiagnostics(type: string, cache: ICache<IDiagnostics[]>, id: string, snapshot: tsTypes.IScriptSnapshot, check: () => tsTypes.Diagnostic[]): IDiagnostics[]
