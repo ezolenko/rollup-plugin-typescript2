7e316b8e811990f1362233479dc18f213ab2ba5b
Anton Gilgur <agilgur5@gmail.com>
2022-07-25 20:59:09 -0400

    make sure to check funcs in context calls too
    - `get-options-overrides`'s coverage func % decreased bc funcs passed to `context.debug` weren't being called
      - took me a bit to notice too since we have no coverage checks
        - and then another bit to realize _why_ it decreased

--- __tests__/fixtures/context.ts
+++ __tests__/fixtures/context.ts
@@ -3,11 +3,19 @@ import { PluginContext } from "rollup";
 
 import { IContext } from "../../src/context";
 
+// if given a function, make sure to call it (for code coverage etc)
+function returnText (message: string | (() => string)) {
+	if (typeof message === "string")
+		return message;
+
+	return message();
+}
+
 export function makeContext(): PluginContext & IContext {
 	return {
-		error: jest.fn(),
-		warn: jest.fn(),
-		info: jest.fn(),
-		debug: jest.fn(),
+		error: jest.fn(returnText),
+		warn: jest.fn(returnText),
+		info: jest.fn(returnText),
+		debug: jest.fn(returnText),
 	} as unknown as PluginContext & IContext;
 };
