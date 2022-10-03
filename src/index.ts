ezolenko/rollup-plugin-typescript2

commit b0e39228b639229ce29d5d8d0b4bd236e4b20852

fix: don't skip resolving files imported by other plugins (#365)
- previously the `allImportedFiles` Set was _basically_ used to skip any files that were imported by other plugins
  - it only filtered out on `importer` (not `importee`)
  - this is a bit of a problem though, as other plugins that create JS code, or `allowJs` JS code, or heck, even regular JS code that Rollup processes without plugins, may actually import TS files
    - and Rollup's plugin system is designed to handle that scenario, so rpt2 actually _should_ resolve those files
      - notably, rpt2 already _transforms_ those files, it just didn't resolve them
        - which is why using `@rollup/plugin-node-resolve` with a `.ts` extension solved this as a workaround
          - the file would be resolved by `node-resolve` then actually transformed by rpt2
          - but rpt2 should resolve TS files itself, `node-resolve` shouldn't be necessary
    - this caused the issue that extensionless imports from such files wouldn't get resolved to TS files and may cause Rollup to error
      - in particular, this popped up with extensionless imports of TS files from Svelte files

- `resolveId` is actually the last remaining place where `allImportedFiles` was used
  - so after removing it from here, we can just remove it entirely
    - which should be a small optimization

- for reference:
  - `allImportedFiles` would be any `include`d files as well as TS files that have been transformed (at this point in the build) and their references
    - this is a pretty gigantic list as is, so I'm actually not sure that removing this is actually that big of a de-opt
      - and it only affects mixed TS / non-TS codebases too
  - this seems to have been added in b0a0ecb5ee8752a1e60962036beb52e9f5dcfff9, but that commit doesn't actually seem to fix the issue it references
    - see my root cause analyses in the issues

src/index.ts
@@ -35,7 +35,6 @@ const typescript: PluginImpl<RPT2Options> = (options) =>
 	let service: tsTypes.LanguageService;
 	let noErrors = true;
 	const declarations: { [name: string]: { type: tsTypes.OutputFile; map?: tsTypes.OutputFile } } = {};
-	const allImportedFiles = new Set<string>();
 
 	let _cache: TsCache;
 	const cache = (): TsCache =>
@@ -103,8 +102,6 @@ const typescript: PluginImpl<RPT2Options> = (options) =>
 
 			if (generateRound === 0)
 			{
-				parsedConfig.fileNames.map(allImportedFiles.add, allImportedFiles);
-
 				context.info(`typescript version: ${tsModule.version}`);
 				context.info(`tslib version: ${tslibVersion}`);
 				if (this.meta)
@@ -156,10 +153,6 @@ const typescript: PluginImpl<RPT2Options> = (options) =>
 
 			importer = normalize(importer);
 
-			// avoiding trying to resolve ids for things imported from files unrelated to this plugin
-			if (!allImportedFiles.has(importer))
-				return;
-
 			// TODO: use module resolution cache
 			const result = tsModule.nodeModuleNameResolver(importee, importer, parsedConfig.options, tsModule.sys);
 			let resolved = result.resolvedModule?.resolvedFileName;
@@ -197,8 +190,6 @@ const typescript: PluginImpl<RPT2Options> = (options) =>
 			if (!filter(id))
 				return undefined;
 
-			allImportedFiles.add(normalize(id));
-
 			const contextWrapper = new RollupContext(pluginOptions.verbosity, pluginOptions.abortOnError, this, "rpt2: ");
 
 			const snapshot = servicesHost.setSnapshot(id, code);
@@ -228,9 +219,6 @@ const typescript: PluginImpl<RPT2Options> = (options) =>
 
 			if (!result)
 				return undefined;
-
-			if (result.references)
-				result.references.map(normalize).map(allImportedFiles.add, allImportedFiles);
 
 			if (watchMode && this.addWatchFile && result.references)
 			{

commit bc1f73b98f153fb1b07a701d25d2c7d47d94a669

fix: don't resolve `filter`ed files
- if they're `exclude`d / not `include`d, then we shouldn't be processing them
  - we're already not transforming them, so this just applies the same exclusion to resolving
  - this is _partly_ a regression from b0e39228b639229ce29d5d8d0b4bd236e4b20852, as that removed the `allImportedFiles` Set that previously filtered out files not in the `tsconfig` `include`
    - but that _itself_ was a regression that was removed -- files that didn't pass `filter` should have _never_ been resolved
      - basically, the `allImportedFiles` regression was covering up this long-standing bug

- also move `.d.ts` check to above the `filter` check
  - we shouldn't be adding declarations to the `cache`, in particular as we don't process declarations, so they'll never be marked as dirty
  - having this check above the `filter` should be slighltly more efficient as well (as would not having these files in the cache graph)

types: be more specific with `filter`'s type
- no need for this to be `any`

src/index.ts
@@ -32,7 +32,7 @@ const typescript: PluginImpl<RPT2Options> = (options) =>
 	let generateRound = 0;
 	let rollupOptions: InputOptions;
 	let context: RollupContext;
-	let filter: any;
+	let filter: ReturnType<typeof createFilter>;
 	let parsedConfig: tsTypes.ParsedCommandLine;
 	let tsConfigPath: string | undefined;
 	let servicesHost: LanguageServiceHost;
@@ -204,11 +204,13 @@ const typescript: PluginImpl<RPT2Options> = (options) =>
 			if (!resolved)
 				return;
 
-			if (filter(resolved))
-				cache.setDependency(resolved, importer);
-
 			if (resolved.endsWith(".d.ts"))
 				return;
+
+			if (!filter(resolved))
+				return;
+
+			cache.setDependency(resolved, importer);
 
 			context.debug(() => `${blue("resolving")} '${importee}' imported by '${importer}'`);
 			context.debug(() => `    to '${resolved}'`);