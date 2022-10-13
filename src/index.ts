70b358b2016b00b279e290a076d97084e991caa3
Anton Gilgur <agilgur5@gmail.com>
2022-10-12 20:40:52 -0400

    fix(build): make our internal Rollup configs TS compatible
    - replace the unmaintained `rollup-plugin-re` w/ `@rollup/plugin-replace`
      - `rollup-plugin-re` hasn't had a commit in ~5 years and doesn't have typings (natively or in DT)
      - also import the plugins in the same order that they're used
    - `require("package.json")` -> `import`
      - and add `resolveJsonModule` to `configPlugin` `tsconfigOverride` to handle this
        - only used by the `configPlugin`, not everything else, so not added to `tsconfig.json`
    
    - fix the usage of the `resolve` plugin as it didn't type-check/had outdated usage

--- src/index.ts
+++ src/index.ts
@@ -16,7 +16,7 @@ import { convertDiagnostic, printDiagnostics } from "./diagnostics";
 import { TSLIB, TSLIB_VIRTUAL, tslibSource, tslibVersion } from "./tslib";
 import { createFilter } from "./get-options-overrides";
 
-// these use globals during testing and are substituted by rollup-plugin-re during builds
+// these use globals during testing and are substituted by @rollup/plugin-replace during builds
 const TS_VERSION_RANGE = (global as any)?.rpt2__TS_VERSION_RANGE || "$TS_VERSION_RANGE";
 const ROLLUP_VERSION_RANGE = (global as any)?.rpt2__ROLLUP_VERSION_RANGE || "$ROLLUP_VERSION_RANGE";
 const RPT2_VERSION = (global as any)?.rpt2__ROLLUP_VERSION_RANGE || "$RPT2_VERSION";
