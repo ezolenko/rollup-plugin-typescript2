85bf27daf524dd1cb7a73deb02ec632707d2540a
Anton Gilgur <agilgur5@gmail.com>
2026-03-29 23:43:32 -0400

    chore(deps): update internal version to 0.37.0, remove temp workaround
    - remove the temp workaround for the `picomatch` 2.3.2 pattern in `rollup.config.js` that was added in e158505af3755755fc2c1573cb4f15cde339dce6
      - this is now built-in with `0.37.0`, so update to that internally

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,6 @@ import ts from "rollup-plugin-typescript2";
 
 import config from "./rollup.config.base";
 
-config.plugins.push(ts({ verbosity: 2, abortOnError: false, include: ["*.ts{,x}", "**/*.ts{,x}", "**/*.cts", "**/*.mts"] }));
+config.plugins.push(ts({ verbosity: 2, abortOnError: false }));
 
 export default config;
